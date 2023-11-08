9871a20afce47ccbcd66670e3a800d5545557258
Jesús de Dios Sánchez <jesusdediossanchez@gmail.com>
2023-11-08 10:24:42 +0100

    Creado formulaio

--- djangotask/task/models.py
+++ djangotask/task/models.py
@@ -5,5 +5,6 @@ class Task(models.Model):
     name= models.CharField(max_length=200)
     description = models.TextField()
     done=models.BooleanField(default=False)
+    
     def __str__(self):
         return self.name
