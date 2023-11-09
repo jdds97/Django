c75f7608f2d6cd90f0f1fb6c8c9a8cdca1577a0c
Jesús de Dios Sánchez <jesusdediossanchez@gmail.com>
2023-11-09 09:00:25 +0100

    Entorno virtual

--- adivina/adivinanumero/models.py
+++ adivina/adivinanumero/models.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 from django.db import models
 from django.db.models import Model
-class Task(models.Model):
+class AdivinaNumero(models.Model):
     numero= models.CharField(max_length=200)
     description = models.TextField()
     done=models.BooleanField(default=False)
     
     def __str__(self):
-        return self.name
+        return self.description
